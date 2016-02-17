79af90e8ae541cab509d264495e2629554c2ace0
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-17 05:52:05 -0500

    lint

--- test.js
+++ test.js
@@ -11,29 +11,29 @@ require('mocha');
 var assert = require('assert');
 var resolveUp = require('./');
 
-describe('glob pattern', function () {
-  it('should return an array of directories:', function () {
+describe('glob pattern', function() {
+  it('should return an array of directories:', function() {
     assert(resolveUp('*').length > 0);
     assert(Array.isArray(resolveUp('*')));
   });
 });
 
-describe('path', function () {
-  it('should return an array of directories:', function () {
+describe('path', function() {
+  it('should return an array of directories:', function() {
     console.time('mocha');
     assert(resolveUp('mocha').length > 0);
     console.timeEnd('mocha');
   });
 
-  it('should return an array of directories:', function () {
+  it('should return an array of directories:', function() {
     console.time('nosymlinks');
-    assert(Array.isArray(resolveUp('mocha', {nosymlinks: true})));
+    assert(Array.isArray(resolveUp('mocha', {fast: true})));
     console.timeEnd('nosymlinks');
   });
 });
 
-describe('errors', function () {
-  it('should throw an error on bad args:', function (cb) {
+describe('errors', function() {
+  it('should throw an error on bad args:', function(cb) {
     try {
       resolveUp();
       cb(new Error('expected an error'));
